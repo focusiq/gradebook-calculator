3548d3adf348c54fc47b81ebd2f62533f8a9447a
Focus Ifeanyi <ifyinbox@gmail.com>
2019-08-05 04:45:55 +0100

    Initial Commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var book = new Book("Scott");
+            book.AddGrade(89.1);
+            book.AddGrade(77.5);
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"The average number is {stats.Average:N1}, the highest number is {stats.High} and the lowest number is {stats.Low}");
+
+
+
+        }
+    }
+}
